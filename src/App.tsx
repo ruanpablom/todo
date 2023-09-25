77c04acaed4b6fb54a3d17593b346dbf1ce45def
Ruan Medeiros <pm.ruan@gmail.com>
2023-09-25 10:49:05 -0300

    refactor: add app title

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TodoReact } from './components/TodoReact';
 function App(): JSX.Element {
   return (
     <div id="app-container" className="w-96">
+      <h1 className="text-white font-bold text-3xl mb-2">TODO List</h1>
       <TodoReact />
     </div>
   );
