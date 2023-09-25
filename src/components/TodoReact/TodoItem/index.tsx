9dc0563ba519365475b31fd48e4e2c8bcb926b4e
Ruan Medeiros <pm.ruan@gmail.com>
2023-09-25 10:19:15 -0300

    feat: drag and drop todo list

--- src/components/TodoReact/TodoItem/index.tsx
+++ src/components/TodoReact/TodoItem/index.tsx
@@ -1,52 +1,65 @@
 import { GoCircle, GoCheckCircle, GoTrash } from 'react-icons/go';
-import { Dispatch, useCallback } from 'react';
+import { Dispatch, forwardRef, useCallback } from 'react';
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from 'react-beautiful-dnd';
 import { Todo } from '../types';
 
 type TodoItemProps = {
   todo: Todo;
   setTodoList: Dispatch<React.SetStateAction<Todo[]>>;
-};
-export function TodoItem({ todo, setTodoList }: TodoItemProps): JSX.Element {
-  const concludeTodo = useCallback(() => {
-    todo.concluded = !todo.concluded;
-    if (todo.concluded) {
-      setTodoList(todoList => [
-        ...todoList.filter(todoItem => todoItem.id !== todo.id),
-        todo,
-      ]);
-    } else {
-      setTodoList(todoList => [
-        todo,
-        ...todoList.filter(todoItem => todoItem.id !== todo.id),
-      ]);
-    }
-  }, [setTodoList, todo]);
+  className?: string;
+} & DraggableProvidedDragHandleProps &
+  DraggableProvidedDraggableProps;
+export const TodoItem = forwardRef<HTMLLIElement, TodoItemProps>(
+  function TodoItem(
+    { className, todo, setTodoList, ...props },
+    ref,
+  ): JSX.Element {
+    const concludeTodo = useCallback(() => {
+      todo.concluded = !todo.concluded;
+      if (todo.concluded) {
+        setTodoList(todoList => [
+          ...todoList.filter(todoItem => todoItem.id !== todo.id),
+          todo,
+        ]);
+      } else {
+        setTodoList(todoList => [
+          todo,
+          ...todoList.filter(todoItem => todoItem.id !== todo.id),
+        ]);
+      }
+    }, [setTodoList, todo]);
 
-  const deleteTodo = useCallback(() => {
-    setTodoList(todoList =>
-      todoList.filter(todoItem => todoItem.id !== todo.id),
+    const deleteTodo = useCallback(() => {
+      setTodoList(todoList =>
+        todoList.filter(todoItem => todoItem.id !== todo.id),
+      );
+    }, [setTodoList, todo.id]);
+
+    return (
+      <li
+        className={`flex gap-2 p-2 w-full bg-white shadowButton rounded items-center ${
+          todo.concluded ? 'line-through decoration-red-700 opacity-75' : ''
+        } ${className || ''}`}
+        {...props}
+        ref={ref}
+      >
+        <span className="font-bold text-red-700 text-left">{todo.content}</span>
+        <div id="buttons-container" className="ml-auto flex gap-2 items-center">
+          <button type="button" onClick={concludeTodo}>
+            {todo.concluded ? (
+              <GoCheckCircle size={20} className="text-red-700" />
+            ) : (
+              <GoCircle size={20} className="text-red-700" />
+            )}
+          </button>
+          <button type="button" onClick={deleteTodo}>
+            <GoTrash size={20} className="text-red-700" />
+          </button>
+        </div>
+      </li>
     );
-  }, [setTodoList, todo.id]);
-
-  return (
-    <li
-      className={`flex gap-2 p-2 w-full bg-white shadowButton rounded items-center ${
-        todo.concluded ? 'line-through decoration-red-700 opacity-75' : ''
-      }`}
-    >
-      <span className="font-bold text-red-700 text-left">{todo.content}</span>
-      <div id="buttons-container" className="ml-auto flex gap-2 items-center">
-        <button type="button" onClick={concludeTodo}>
-          {todo.concluded ? (
-            <GoCheckCircle size={20} className="text-red-700" />
-          ) : (
-            <GoCircle size={20} className="text-red-700" />
-          )}
-        </button>
-        <button type="button" onClick={deleteTodo}>
-          <GoTrash size={20} className="text-red-700" />
-        </button>
-      </div>
-    </li>
-  );
-}
+  },
+);
--- src/components/TodoReact/TodoList/index.tsx
+++ src/components/TodoReact/TodoList/index.tsx
@@ -1,4 +1,11 @@
 import { Dispatch } from 'react';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from 'react-beautiful-dnd';
+
 import { TodoItem } from '../TodoItem';
 import { Todo } from '../types';
 
@@ -8,11 +15,52 @@ type TodoListProps = {
 };
 
 export function TodoList({ todos, setTodoList }: TodoListProps): JSX.Element {
+  const handleDrop = (droppedItem: DropResult) => {
+    // Ignore drop outside droppable container
+    if (!droppedItem.destination) return;
+
+    setTodoList(todoList => {
+      const updatedList = [...todoList];
+
+      const [reorderedItem] = updatedList.splice(droppedItem.source.index, 1);
+      updatedList.splice(droppedItem.destination!.index, 0, reorderedItem);
+
+      return updatedList;
+    });
+  };
+
   return (
-    <ul className="flex flex-col gap-3">
-      {todos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} setTodoList={setTodoList} />
-      ))}
-    </ul>
+    <DragDropContext onDragEnd={handleDrop}>
+      <Droppable droppableId="list-container">
+        {provided => (
+          <ul
+            className="flex flex-col gap-3 list-container"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {todos.map((item, index) => (
+              <Draggable key={item.id} draggableId={item.id} index={index}>
+                {provided => (
+                  <TodoItem
+                    className="item-container"
+                    todo={item}
+                    setTodoList={setTodoList}
+                    ref={provided.innerRef}
+                    {...provided.dragHandleProps!}
+                    {...provided.draggableProps!}
+                  />
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
+    // <ul className="flex flex-col gap-3">
+    //   {todos.map(todo => (
+    //     <TodoItem key={todo.id} todo={todo} setTodoList={setTodoList} />
+    //   ))}
+    // </ul>
   );
 }
