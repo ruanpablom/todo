6f25e23d450a323f8eaecf43e59ce1c9a0a07c4d
Ruan Medeiros <pm.ruan@gmail.com>
2023-09-25 01:03:32 -0300

    feat: localstora todoList

--- src/components/TodoReact/index.tsx
+++ src/components/TodoReact/index.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Todo } from './types';
 import { TodoInput } from './TodoInput';
 import { TodoList } from './TodoList';
 
 export function TodoReact(): JSX.Element {
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [todoList, setTodoList] = useState<Todo[]>(
+    localStorage.getItem('todoList')
+      ? JSON.parse(localStorage.getItem('todoList') as string)
+      : [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, [todoList]);
 
   return (
     <div id="todo-container" className="w-full">
