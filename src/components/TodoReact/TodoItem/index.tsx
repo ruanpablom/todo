b9f6c43f9a3ac5f75a1a233f971f0e6937234eb0
Ruan Medeiros <pm.ruan@gmail.com>
2023-09-25 00:51:54 -0300

    feat: conclude and delete todo functions

--- src/components/TodoReact/TodoItem/index.tsx
+++ src/components/TodoReact/TodoItem/index.tsx
@@ -1,5 +1,5 @@
 import { GoCircle, GoCheckCircle, GoTrash } from 'react-icons/go';
-import { Dispatch } from 'react';
+import { Dispatch, useCallback } from 'react';
 import { Todo } from '../types';
 
 type TodoItemProps = {
@@ -7,12 +7,18 @@ type TodoItemProps = {
   setTodoList: Dispatch<React.SetStateAction<Todo[]>>;
 };
 export function TodoItem({ todo, setTodoList }: TodoItemProps): JSX.Element {
-  const concludeTodo = (todo: Todo) => {
+  const concludeTodo = useCallback(() => {
     todo.concluded = !todo.concluded;
     setTodoList(todoList =>
       todoList.map(todoItem => (todoItem.id === todo.id ? todo : todoItem)),
     );
-  };
+  }, [setTodoList, todo]);
+
+  const deleteTodo = useCallback(() => {
+    setTodoList(todoList =>
+      todoList.filter(todoItem => todoItem.id !== todo.id),
+    );
+  }, [setTodoList, todo.id]);
 
   return (
     <li
@@ -23,14 +29,14 @@ export function TodoItem({ todo, setTodoList }: TodoItemProps): JSX.Element {
     >
       <span className="font-bold text-red-700 text-left">{todo.content}</span>
       <div id="buttons-container" className="ml-auto flex gap-2 items-center">
-        <button type="button" onClick={() => concludeTodo(todo)}>
+        <button type="button" onClick={concludeTodo}>
           {todo.concluded ? (
             <GoCheckCircle size={20} className="text-red-700" />
           ) : (
             <GoCircle size={20} className="text-red-700" />
           )}
         </button>
-        <button type="button">
+        <button type="button" onClick={deleteTodo}>
           <GoTrash size={20} className="text-red-700" />
         </button>
       </div>
